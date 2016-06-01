47b9f12b1789e37ff15f335b1a304454337def5a
Pavle Mijatovic <paul.mijatovic@gmail.com>
2016-06-01 16:52:53 +0200

    Test 1

--- aLIB/ViewController.swift
+++ aLIB/ViewController.swift
@@ -7,12 +7,63 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+  
+  
+  let audioEngine  = AVAudioEngine()
+  
+  
+  
+  override func loadView() {
+    super.loadView()
+    
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+  }
+  
+  
+  
+  @IBAction func test(sender: UIButton) {
+    let inputNode = audioEngine.inputNode
+    let bus = 0
+    inputNode!.installTapOnBus(bus, bufferSize: 2048, format: inputNode!.inputFormatForBus(bus)) {
+      (buffer: AVAudioPCMBuffer!, time: AVAudioTime!) -> Void in
+      
+      
+      let firstChannel = buffer.floatChannelData[0]
+      
+
+      
+      
+      let arr = Array(UnsafeBufferPointer(start: firstChannel, count: Int(buffer.frameLength)))
+      
+      
+      
+
+//      print(arr)
+      print(buffer.floatChannelData)
+      print("")
+
+//      print(buffer.floatChannelData[50])
+    }
+    
+    audioEngine.prepare()
+    do{
+      try audioEngine.start()
+    }catch{
+      
+      print("Error")
+      
+    }
+    
   }
 }
 
+
+
+//let firstChannel = buffer.floatChannelData[0]
+//let arr = Array(UnsafeBufferPointer(start: firstChannel, count: Int(buffer.frameLength)))
+//// Do something with your array of Floats
---
+++ aLIB/ViewController2.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController2.swift
+//  aLIB
+//
+//  Created by Pavle Mijatovic on 6/1/16.
+//  Copyright © 2016 Pavle Mijatovic. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+      
+      
+
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+  @IBAction func vc2Test(sender: UIButton) {
+    
+    let audioEngine = AVAudioEngine()
+    
+    let inputNode = audioEngine.inputNode
+    let sampleCount = 2048
+    let bus = 0
+    var samplesAsDoubles = Array(count: Int(sampleCount), repeatedValue: CDouble())
+    let frameLength = UInt32(sampleCount) // This seems to be ignored when passed into the AudioTap, therefore reassign within block.
+    inputNode!.installTapOnBus(0, bufferSize:frameLength, format: inputNode!.inputFormatForBus(bus), block: { (
+      buffer: AVAudioPCMBuffer!,
+      audioTime  : AVAudioTime!) in
+      
+      // Change incomming buffer size
+      buffer.frameLength = UInt32(sampleCount)
+      
+      // Populate array with incomming audio samples
+      for var i = 0; i < Int(buffer.frameLength); i++
+      {
+        samplesAsDoubles[i] = Double(buffer.floatChannelData.memory[i])
+        
+      }
+      //Probably the Groeztel function needs to be called from somewhere here
+    })
+    
+    
+  }
+
+}
